e83e3ca6e6cf6ac4d9d2d14e8ec51688b22c8ded
Olin Kirkland <olin.kirkland@beg.de>
2024-07-09 14:48:53 +0200

    Add API tests

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,17 @@
-console.log('foo');
+import express from 'express';
+const app = express();
+const port = 3000;
 
-const foo = 'bar';
+app.use(express.json());
 
-function foobar() {
-    console.log('foobar');
-}
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello, world!' });
+});
+
+app.post('/api/echo', (req, res) => {
+    res.json({ data: req.body });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
