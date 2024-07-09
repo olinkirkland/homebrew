f6201f37133e4cbba44e8da9f8a9c20b95fcdf07
Olin Kirkland <olin.kirkland@beg.de>
2024-07-09 13:57:01 +0200

    Remove eslint from the client, will add a linter eventually when I am in the mood

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,15 +3,3 @@ import './style.css';
 import App from './App.vue';
 
 createApp(App).mount('#app');
-
-let didShowMessage = false;
-
-function showMessage() {
-    const message = 'I like logs';
-
-    for (let i = 0; i < 5; i++) {
-        console.log(message);
-    }
-}
-
-showMessage();
