752133e2a0e3594ff9521fb179cd480f1932c974
Olin Kirkland <olin.kirkland@beg.de>
2024-07-09 15:40:51 +0200

    Update api-test.http

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -3,7 +3,22 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended
-);
+export default [
+    ...tseslint.configs.recommended,
+    eslint.configs.recommended,
+    {
+        env: {
+            mocha: true, // Add Mocha environment
+            node: true,
+            es6: true,
+        },
+        globals: {
+            describe: 'readonly',
+            it: 'readonly',
+            before: 'readonly',
+            after: 'readonly',
+            beforeEach: 'readonly',
+            afterEach: 'readonly',
+        },
+    },
+];
